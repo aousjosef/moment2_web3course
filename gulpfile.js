19ba6b57000d05156865bc6d3426b36e11c324dc
aousjosef <aoussabah@gmail.com>
2023-04-05 02:16:23 +0200

    renamed folders

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const cssnano = require("gulp-cssnano");
+const terser = require("gulp-terser");
+
+const files = {
+  htmlPath: "src/**/*.html",
+  jsPath: "src/**/*.js",
+  scssPath: "src/**/*.scss",
+};
+
+// html Task
+function htmlTask() {
+  return src(files.htmlPath).pipe(dest("pub"));
+}
+
+//scss task
+function scssTask() {
+  return src(files.scssPath) //väljer scss filer
+    .pipe(sass()) //konvertera scss till css
+    .pipe(concat("main_style.css")) //slår ihop ihop scss filerna
+    .pipe(cssnano()) //miniferar css
+    .pipe(dest("pub/css")); //publicerar till pub mappen
+}
+
+function jsTask() {
+  return src(files.jsPath) //väljer js filer
+    .pipe(concat("main.js"))
+    .pipe(terser())
+    .pipe(dest("pub/js")); //publicerar till
+}
+
+//Browsersync server start
+
+function browsersyncServer(cb) {
+  browserSync.init({
+    server: {
+      baseDir: "pub",
+    },
+  });
+  cb();
+}
+
+//Browsersync reload
+function browsersyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+//watch task
+function watchTask() {
+  watch(
+    [files.htmlPath, files.scssPath, files.jsPath],
+    series(parallel(htmlTask, scssTask, jsTask), browsersyncReload)
+  );
+}
+
+exports.default = series(
+  parallel(htmlTask, scssTask, jsTask),
+
+  browsersyncServer,
+  watchTask
+);
---
+++ src/js/jsfile_0.js
@@ -0,0 +1,1 @@
+let msg = "Message after merges, changed branch to exp";
