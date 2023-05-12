b5f4f06e195b2af0d9638fb09f58935fd84a7a9e
aousjosef <aoussabah@gmail.com>
2023-05-13 01:11:37 +0200

    made menu responsive

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,1 @@
-let msg="Message after merges, changed branch to exp2";console.log(msg);
+let msg="Message after merges, changed branch to exp2";console.log(msg);var x_button=document.getElementById("x_button"),nav=document.getElementById("nav"),menu_button=document.getElementById("menu_button");x_button.addEventListener("click",(function(){nav.classList.toggle("menu-open")})),menu_button.addEventListener("click",(function(){nav.classList.toggle("menu-open")}));
--- src/js/jsfile_1.js
+++ src/js/jsfile_1.js
@@ -1,1 +1,13 @@
 console.log(msg);
+
+var x_button = document.getElementById("x_button");
+var nav = document.getElementById("nav");
+var menu_button = document.getElementById("menu_button");
+
+x_button.addEventListener("click", function () {
+  nav.classList.toggle("menu-open");
+});
+
+menu_button.addEventListener("click", function () {
+  nav.classList.toggle("menu-open");
+});
